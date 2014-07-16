987f0c28670adb90c99bc48b8ebb7f72f2005aed
John Mock <john.mock@gmail.com>
2014-07-15 22:36:47 -0400

    removed image type validation because it's not working on heroku

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -7,7 +7,6 @@ class Pin < ActiveRecord::Base
 	validates :description, presence: true #for others search for rails validations
 	validates :user_id, presence: true
 	validates_attachment :image, presence: true,
-							content_type: {content_type: ['image/jpeg','image/jpg','image/png','image/gif']},
 							size: { less_than: 5.megabytes}
 	belongs_to :user
 
